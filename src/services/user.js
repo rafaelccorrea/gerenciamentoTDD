a74628c6c51836b4a94bc5081367623e2b58a119
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2023-07-05 12:57:08 -0300

    Implements validations user

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,19 @@
 module.exports = (app) => {
-  const findAll = () => app.db('users').select();
+  const findAll = (filter = {}) => app.db('users').where(filter).select();
 
-  const save = (user) => app
-    .db('users')
-    .insert(user, '*');
+  const save = async (user) => {
+    if (!user.name) return { error: 'Name is required' };
+    if (!user.email) return { error: 'Email is required' };
+    if (!user.password) return { error: 'Password is required' };
+
+    const userDb = await findAll({ email: user.email });
+
+    if (userDb && userDb.length > 0) return { error: 'Email exists already' };
+
+    return app
+      .db('users')
+      .insert(user, '*');
+  };
 
   return {
     findAll,
--- test/user.test.js
+++ test/user.test.js
@@ -2,14 +2,15 @@ const request = require('supertest');
 
 const app = require('../src/app');
 
+const mail = `${Date.now()}@mail.com`;
+
 test('List all users', () => request(app).get('/users').then((res) => {
   expect(res.status).toBe(200);
   expect(res.body.length).toBeGreaterThan(0);
 }));
 
-test('Create a new user', () => {
-  const mail = `${Date.now()}@mail.com`;
-  return request(app).post('/users')
+test('Create a new user', () =>
+  request(app).post('/users')
     .send({
       name: 'Jessica',
       email: mail,
@@ -17,5 +18,52 @@ test('Create a new user', () => {
     })
     .then((res) => {
       expect(res.status).toBe(201);
+    }));
+
+test('User name not found!', () => request(app).post('/users')
+  .send({
+    email: mail,
+    password: '123456',
+  })
+  .then((res) => {
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Name is required');
+  }));
+
+test('User email not found!', async () => {
+  const result = await request(app).post('/users')
+    .send({
+      name: 'Jessica',
+      password: '123456',
     });
+
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email is required');
+});
+
+test('User password not found!', (done) => {
+  request(app).post('/users')
+    .send({
+      name: 'Jessica',
+      email: mail,
+    })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Password is required');
+      done();
+    })
+    .catch((err) => {
+      done.fail(err);
+    });
+});
+
+test('User email exists!', async () => {
+  const result = await request(app).post('/users')
+    .send({
+      name: 'Jessica',
+      email: mail,
+      password: '123456',
+    });
+  expect(result.status).toBe(400);
+  expect(result.body.error).toBe('Email exists already');
 });
