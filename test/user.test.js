6bdecc4b02cb69233f13986552ca18b995fcaa2b
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2023-08-30 23:35:13 -0300

    adjs token to tests user

--- test/user.test.js
+++ test/user.test.js
@@ -1,13 +1,30 @@
 const request = require('supertest');
+const jwt = require('jwt-simple');
 
 const app = require('../src/app');
 
 const mail = `${Date.now()}@mail.com`;
+const userEmailMaster = `${Date.now()}@example.com`;
+const secret = 'secret!';
 
-test('List all users', () => request(app).get('/users').then((res) => {
-  expect(res.status).toBe(200);
-  expect(res.body.length).toBeGreaterThan(0);
-}));
+let user;
+
+beforeAll(async () => {
+  const res = await app.services.user.save({
+    name: 'User account',
+    email: userEmailMaster,
+    password: '123456',
+  });
+  user = { ...res[0] };
+  user.token = jwt.encode(user, secret);
+});
+
+test('List all users', () => request(app).get('/users')
+  .set('authorization', `bearer ${user.token}`)
+  .then((res) => {
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBeGreaterThan(0);
+  }));
 
 test('Should create user and encrypt password', async () => {
   const result = await request(app).post('/users')
@@ -15,8 +32,7 @@ test('Should create user and encrypt password', async () => {
       name: 'Jessica',
       email: mail,
       password: '123456',
-    });
-
+    }).set('authorization', `bearer ${user.token}`);
   expect(result.status).toBe(201);
 
   const { id } = result.body;
@@ -29,7 +45,7 @@ test('User name not found!', () => request(app).post('/users')
   .send({
     email: mail,
     password: '123456',
-  })
+  }).set('authorization', `bearer ${user.token}`)
   .then((res) => {
     expect(res.status).toBe(400);
     expect(res.body.error).toBe('Name is required');
@@ -40,7 +56,7 @@ test('User email not found!', async () => {
     .send({
       name: 'Jessica',
       password: '123456',
-    });
+    }).set('authorization', `bearer ${user.token}`);
 
   expect(result.status).toBe(400);
   expect(result.body.error).toBe('Email is required');
@@ -51,7 +67,7 @@ test('User password not found!', (done) => {
     .send({
       name: 'Jessica',
       email: mail,
-    })
+    }).set('authorization', `bearer ${user.token}`)
     .then((res) => {
       expect(res.status).toBe(400);
       expect(res.body.error).toBe('Password is required');
@@ -68,7 +84,7 @@ test('User email exists!', async () => {
       name: 'Jessica',
       email: mail,
       password: '123456',
-    });
+    }).set('authorization', `bearer ${user.token}`);
   expect(result.status).toBe(400);
   expect(result.body.error).toBe('Email exists already');
 });
